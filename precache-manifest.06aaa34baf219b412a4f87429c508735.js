af7c4392ad8229863d406fe6bd5cc1f72e16ca4b
Tommy Pickles <jhawke2546@gmail.com>
2022-09-19 11:19:49 +1000

    Updates

---
+++ precache-manifest.06aaa34baf219b412a4f87429c508735.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "019b94e5e4e62cc6aed7",
+    "url": "//portfolio-website/static/css/main.e3579750.chunk.css"
+  },
+  {
+    "revision": "019b94e5e4e62cc6aed7",
+    "url": "//portfolio-website/static/js/main.4a42896b.chunk.js"
+  },
+  {
+    "revision": "def492f2469b76cd44d9",
+    "url": "//portfolio-website/static/js/runtime~main.62161e32.js"
+  },
+  {
+    "revision": "1a0bad3f33e17ec1e092",
+    "url": "//portfolio-website/static/js/2.fc51626d.chunk.js"
+  },
+  {
+    "revision": "47124345dc01232adae5fe342d9c7b7c",
+    "url": "//portfolio-website/index.html"
+  }
+];
