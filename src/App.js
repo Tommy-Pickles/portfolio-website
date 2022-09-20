bb0640488f59a8ba4c6447a3e7bb2d696e983e6f
Tommy Pickles <jhawke2546@gmail.com>
2022-09-20 17:19:39 +1000

    deploy changes

--- src/App.js
+++ src/App.js
@@ -153,7 +153,6 @@ function App() {
 
   ]
 
- 
 
   return (
     <>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,25 @@ import { Button } from './Button';
 import'./Navbar.css'
 
 function Navbar() {
-  const[click, setClick] =useState(false);
-  const[button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick = (false);
+  const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if(window.innerWidth <=960){
+    if (window.innerWidth <= 960) {
       setButton(false);
     } else {
       setButton(true);
     }
   };
 
-  useEffect(() =>{
-    showButton()
+  useEffect(() => {
+    showButton();
   }, []);
 
-window.addEventListener('resize', showButton);
+  window.addEventListener('resize', showButton);
 
   return (
     <>
