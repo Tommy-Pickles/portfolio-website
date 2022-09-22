94404c13b38df925154871292353482998dbd049
Tommy Pickles <jhawke2546@gmail.com>
2022-09-22 13:14:57 +1000

    image changes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,7 @@ import './Cards.css';
 function Cards() {
   return (
     <div className='cards'>
-        <h1>Categories </h1> 
+        <h1>Categoriez </h1> 
         <div className='cards__container'>
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
