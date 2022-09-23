ab3f919af0f468a1ccfe93e2db22ba2892b9e5e3
Tommy Pickles <jhawke2546@gmail.com>
2022-09-24 07:20:15 +1000

    navbar troubleshooting

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
+// import { Button } from './Button';
 import './HeroSection.css';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>PORTFOLIO</h1>
-      <p>A collection of my Memes, Writing and Art</p>
+      <h1>PORTFOLIOT</h1>
+      <p>A collection of my Memes, Writing and Art1</p>
       {/* <div className='hero-btns'>
         <Button
           className='btns'
