8b71f0c42451529525d22406ec1a893862672447
Tommy Pickles <jhawke2546@gmail.com>
2022-10-05 17:45:50 +1100

    text changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>PORTFOLIO</h1>
-      <p>A collection of my Memes, Writing and Art</p>
+      <p>Memes, Writing and Art</p>
       {/* <div className='hero-btns'>
         <Button
           className='btns'
